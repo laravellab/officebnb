27359f65f5277ffdbe8dd538bda1a37f16f50328
Yash Pal <iyashpal.dev@gmail.com>
2021-09-14 11:57:21 +0530

    Factories setup complete.

--- app/Models/Office.php
+++ app/Models/Office.php
@@ -13,6 +13,11 @@ class Office extends Model
 {
     use HasFactory, SoftDeletes;
 
+
+    const APPROVAL_PENDING = 1;
+    const APPROVAL_APPROVED = 2;
+    const APPROVAL_REJECTED = 3;
+
     protected $casts = [
         'lat' => 'decimal:8',
         'lat' => 'decimal:8',
