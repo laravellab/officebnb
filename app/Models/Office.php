83ca322f26265d0e8a3b8f58776d46b7e6a41211
Yash Pal <iyashpal.dev@gmail.com>
2021-09-14 11:48:54 +0530

    Model preparation complete.

--- app/Models/Office.php
+++ app/Models/Office.php
@@ -5,8 +5,35 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\MorphMany;
 
 class Office extends Model
 {
     use HasFactory, SoftDeletes;
+
+    protected $casts = [
+        'lat' => 'decimal:8',
+        'lat' => 'decimal:8',
+        'approval_status' => 'integer',
+        'hidden' => 'bool',
+        'price_per_day' => 'integer',
+        'monthly_discount' => 'integer',
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function reservations(): HasMany
+    {
+        return $this->hasMany(Reservation::class);
+    }
+
+    public function images(): MorphMany
+    {
+        return $this->morphMany(Image::class, 'resource');
+    }
 }
